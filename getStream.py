c621938f10beae71691cf26108d75ddb2fc4aa1a
Satvik <satvik.jagannath7@gmail.com>
2022-07-24 16:22:53 +0530

    Update getStream.py

--- getStream.py
+++ getStream.py
@@ -32,10 +32,10 @@ if __name__ == '__main__':
     listener = TweetsStreamDataListener()
 
     # set twitter keys/tokens
-    consumer_key = "RK6Gn5h3cq1llfXTz1zY7Hsjx"
-    consumer_secret = "X6cqtrIH6Pe8HFk5I9MaKmcW0SjsEksqk8b3fUfcL9L4Vye4LV"
-    access_token = "206985484-ZR2aQPnxdzBOpSeuobtH3dDLkO4NLhJRwwGLtrmQ"
-    access_token_secret = "lUerNXEmhVlXydVi03tn3LWNPcp1Iy9vaBMeLH2QmRIFZ"
+    consumer_key = "SETKEY"
+    consumer_secret = "SETSECRET"
+    access_token = "SETACCESSTOKEN"
+    access_token_secret = "SETACCESSTOKENSECRET"
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, listener)
